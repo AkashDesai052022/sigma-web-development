c33899940d22f91e01f2cfecb13c3d211ad2149f
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-18 13:24:13 +0530

    variables, data types and objects

---
+++ video55/script.js
@@ -0,0 +1,85 @@
+console.log("Hey this is tutorial video 55")
+
+// var a = 5;
+// var b = 6;
+// var c = "Harry"
+
+// console.log(a + b + c)
+
+// console.log(typeof a, typeof b, typeof c)
+
+// var  a = 10;
+// if (true){
+//    var a = 5;
+//     console.log(a)
+// }
+// console.log(a)
+
+let b1 = 5;
+b1 = b1 + 5
+console.log(b1)
+
+
+let one = "harry bhai"
+let two = 23;
+let three = 3.565
+let four = true;
+let five = undefined;
+let six = null;
+
+console.log(one, two, three, four, five, six)
+console.log(typeof one, typeof two, typeof three, typeof four, typeof five, typeof six)
+
+
+// object
+
+let obj = {
+    'name': "akash",
+    'job role': "web developer",
+    'isHandsome': true,
+}
+console.log(obj)
+// if you want to add key to object then
+obj.salary = "1234567"
+console.log(obj)
+
+
+const student_1 = {
+    'name': "harry",
+    'phone number': 9594088791,
+    'total marks': "95/100",
+}
+console.log(student_1)
+const student_2 = {
+    'name': "akash",
+    'phone number': 9326501533,
+    'total marks': "85/100",
+}
+console.log(student_2)
+const student_3 = {
+    'name': "anup",
+    'phone number': 9561023871,
+    'total marks': "99/100",
+}
+console.log(student_3)
+
+// chapter 1 quiz
+
+// question 1 : create a variable of type string and try to add number to it.
+let str = 'akash';
+console.log(str + 4)
+
+// question 2 : use typeof operator to find the datatype of the string in last question.
+console.log(typeof str)
+
+// question 3: create a const object in js can you change it to hold a number later?
+const obj2 = {
+    name : 'akash'
+}
+
+// question 4 : try to add a new key to the const object in problem 3 were you able to do it?
+
+const obj3 = {
+    name : 'akash',
+    num : 2,
+}
