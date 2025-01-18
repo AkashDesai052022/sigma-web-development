9959a48253bc761aebea0d3896d2d05b7bdb8c84
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-18 12:05:54 +0530

    Introduction to JavaSript

---
+++ video54/new.js
@@ -0,0 +1,4 @@
+console.log("Hello World!!!")
+
+document.title = 'akash'
+document.body.style.backgroundColor = 'red'
---
+++ video54/script.js
@@ -0,0 +1,16 @@
+alert("hi, I am akash")
+
+
+console.log("code is running...")
+// for now harry bhaiya says, use var keyword for variable and we will sutdy it later 
+var a = prompt('enter a number')
+
+console.log('your number is ' + a)
+
+var isTrue = confirm("Are you sure about leave this page and blast your computer")
+if(isTrue){
+    console.log("your computer will blasting soon...")
+}
+else{
+    console.log("your computer is not blasting ")
+}
