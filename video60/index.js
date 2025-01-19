85ceb402b1b3ce505a926b12ecc466eab515fd16
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-20 01:41:55 +0530

    JS strings

---
+++ video60/index.js
@@ -0,0 +1,26 @@
+console.log("hey this is string tutorial")
+let a = "harry"
+console.log(a[0]);
+console.log(a[1]);
+console.log(a[2]);
+console.log(a[3]);
+console.log(a[4]);
+//console.log(a[5]);   this will throw error : undefined
+
+
+// template literals
+
+let real_name = "harry"
+let friend = "rohan"
+
+console.log("his name is "+ real_name + ' ' + "and his friend name is " + friend)
+// now the entry of real hero 
+console.log(`his name is ${real_name} and his friend name is ${friend}`)
+
+
+// more about strings
+
+let b = "Shivam"
+console.log(b.toUpperCase())
+console.log(b.toLowerCase())
+console.log(b.length)
