8015e5abd0c5a53dee088fb791043428bf9ec5b1
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-19 12:44:51 +0530

    JavaScript Conditionals: if, else if, else ladder

---
+++ video56/index.js
@@ -0,0 +1,42 @@
+// console.log("hi I am web developer")
+
+// let age = 4;
+
+// if(age > 18){
+//     console.log("you can drive")
+// }
+// else{
+//     console.log('you cannot drive')
+// }
+
+// let grace = 2;
+
+// console.log(a + grace)
+// console.log(a - grace)
+// console.log(a / grace)
+// console.log(a * grace)
+// console.log(a ** grace)
+// console.log(a % grace)
+
+// console.log(a+= grace)
+// console.log(a) // after this increment and decrement operator use the original value of a is changes permenantly.
+// console.log(a -= grace)
+// console.log(a % grace)
+
+// let a = 4;
+
+// if(a == 3){
+// console.log("the number is 3")
+// }
+// else if(a == 4){
+//     console.log("yes you are right.")
+// }
+// else{
+//     console.log('the number is not 3')
+// }
+
+let a = 5;
+let b = 7;
+
+let c = a > b ? a - b : b - a;
+console.log(c);
