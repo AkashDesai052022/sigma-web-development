4b50ba5bf82a1a4716dcbba70df66deecbf25f29
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-21 01:50:47 +0530

    exercise10 try by me

---
+++ video62/index.js
@@ -0,0 +1,14 @@
+// create a business names
+
+let str1 =  "crazy"
+let str2 = "Amazing"
+let str3 = "fire"
+let str4 = "Engine" 
+let str5 = "Foods" 
+let str6 = "Garments"
+let str7 = "Bros"
+let str8 = "Limited"
+let str9 = "Hub"
+
+let business_name = str1.concat( " ",...str4, " ", str7)
+console.log(business_name)
