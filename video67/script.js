e09af8c9ef8a687cde026bffa0afdede68f3fbe1
AkashDesai052022 <akashdesai052022@gmail.com>
2025-01-23 01:00:08 +0530

    JavaScript DOM - Children, Parent & Sibling Nodes

---
+++ video67/script.js
@@ -0,0 +1,10 @@
+console.log("hello world")
+
+
+
+document.body.firstChild
+document.body.firstChild.childNodes
+document.body.firstChild.children // gives a html collection of element
+
+// want parent element
+document.body.firstElementChild.children[3].parentElement
